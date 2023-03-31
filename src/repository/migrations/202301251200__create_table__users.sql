dd3512ab2866bf0e4660fe3a95393edba1e60bdf
InmediaGR <inmediagreece@gmail.com>
2023-03-31 23:29:43 +0300

    Update 202301251200__create_table__users.sql

--- src/repository/migrations/202301251200__create_table__users.sql
+++ src/repository/migrations/202301251200__create_table__users.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
   id          SERIAL PRIMARY KEY,
-  username    VARCHAR(64) NOT NULL UNIQUE,
-  password    VARCHAR(64) NOT NULL,
+  email    VARCHAR(64) NOT NULL UNIQUE,
+  username    VARCHAR(64) DEFAULT NULL UNIQUE,
+  password    VARCHAR(64) DEFAULT NULL,
+  password_hash  VARCHAR(64) DEFAULT NULL,
   deleted_at  TIMESTAMPTZ DEFAULT NULL
 );
